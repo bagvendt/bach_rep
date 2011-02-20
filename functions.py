56b33e6c2c479d22ae2f3457b375dde5f3641a15
Marcus <mgmano@gmail.com>
2011-02-20 17:41:58 +0100

    Some rewriting of stuff, and documentation

--- functions.py
+++ functions.py
@@ -6,6 +6,15 @@ import pylab
 from scipy.ndimage import gaussian_filter
 from proc import *
 
+"""
+When implementing new functions functions should be of the form
+fname(img,env, **kwargs)
+and fname should return the tuple (img,env)
+"""
+
+
+
+
 def setup(image,env):
 	"""Loads the image and sets up the environment"""
 	#flatten=1 makes image grayscaled.
@@ -13,13 +22,20 @@ def setup(image,env):
 	return (img,env)
 
 def display(img,env,**kwargs):
+	"""Displays the image
+	This function has some limitations as it is only possible to
+	show one image per time the script is run.
+	Futhermore the script returns the tuple (img,env) which doesn't make
+	any sense atm.
+	"""
 	pylab.figure()
 	pylab.imshow(img)
 	pylab.show()
 	return (img,env)
 
-def abss(img,env,**kwargs):
-	img = abs(img)
+def abs_func(img,env,**kwargs):
+	#Remember that abs is correcly manipulating the values piecewise
+	img = numpy.abs(img)
 	return (img,env)
 
 def fft2(img,env,**kwargs):
@@ -27,9 +43,8 @@ def fft2(img,env,**kwargs):
 	return (img,env)
 
 def gauss(img,env,**kwargs):
-	#Remember that abs is correcly manipulating the values piecewise
 	value = kwargs['value']
-	img = gaussian_filter(abs(img),value)
+	img = gaussian_filter(img,value)
 	return (img,env)
 
 def ifft2(img,env,**kwargs):
--- proc.py
+++ proc.py
@@ -1,11 +1,20 @@
 from functions import *
 
-Claes_test = ( 
+test1 = ( 
 	'../img/celle1.png',
 	[(fft2,{}),
+	(abs_func,{}),
 	(gauss,{'value':0.0044,}),
 	(fftshift,{}),
 	(display,{})],
 	)
 
-func_list = [Claes_test]
+test2 = ( 
+	'../img/celle1.png',
+	[(fft2,{}),
+	(abs_func,{}),
+	(display,{})],
+	)
+
+#Remember to add new procedures to this list
+func_list = [test1,test2]
