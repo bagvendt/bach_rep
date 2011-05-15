8bac716494f5ea3ed2b065eb36c097bfcaaa893a
Claes Ladefoged <claesladefoged@Claes.local>
2011-05-15 16:53:19 +0200

    Edited a file

--- DictAndConvCombined.m
+++ DictAndConvCombined.m
@@ -1,10 +1,16 @@
 close all;
 clear all;
+
+base1 = '/Users/claesladefoged/Dropbox/Skole/3/Bachelor/Billeder/csgb/';
+%base1 = '/home/marcus/Dropbox/Bachelor/Billeder/csgb/';
+base2 = '/Users/claesladefoged/Documents/bach_rep/images/vessikels/';
+%base2 = '/home/marcus/Projekter/bach_rep/images/vessikels/';
+
 n=1;
 percentage=0.35;
 fntsz=18;
 
-I = double(imread('/Users/claesladefoged/Dropbox/Skole/3/Bachelor/Billeder/csgb/test20_58.jpg'));
+I = double(imread(strcat(base1,'test20_58.jpg')));
 figure(n); n=n+1;
 imagesc(I); axis image; colormap(gray); colorbar
 set(gca,'fontsize',fntsz);
@@ -22,60 +28,76 @@ imagesc(double(I)); axis image; colormap(gray); colorbar
 set(gca,'fontsize',fntsz);
 title('Cuttet image');
 
-J = double(imread('/Users/claesladefoged/Documents/bach_rep/images/vessikels/ves6.png'));
+J = double(imread(strcat(base2,'ves6.png')));
 fun = @(x) sum(((x(:)-mean(x(:)))/std(x(:))-(J(:)-mean(J(:)))/std(J(:))).^2);
 K=nlfilter(I,size(J),fun);
-figure(n); n=n+1;
+
 convImg = (K-min(K(:)))<percentage*(max(K(:))-min(K(:)));
+figure(n); n=n+1;
 imagesc(convImg); axis image; colormap(gray); colorbar
 set(gca,'fontsize',fntsz);
 title('convImg');
 
 % DO USE OF DICT HERE
 
-dictImg = I.*0;
-[h,w] = size(I);
+load('minmis');
 
+NewI = I.*0;
+
+[h,w] = size(I);
+[i1,c,i3] = size(mat);
+
+% For each height pixel except the last 7 (size of cut)
 for i = 1:(h-7)
     strcat(num2str(round((i/(h-7))*100)),'%')
+    % For each width pixel except the last 7
     for j = 1:(w-7)
-        cut = double(I(i+[0:7],j+[0:7]));
+        cut = double(I(i+[0:6],j+[0:6]));
+        cut = reshape(cut,1,i3);
         shortest_dist = 10^10;
-        shortest_name = '';
-        for x = 0:3
-            for y=0:4
-                name1 = ['orig_',num2str(x),'_',num2str(y),'.txt'];
-                patch = load(name1);
+        % Run through each cut with x
+        for x = 1:i1
+
+                
+                patch = mat(x,1,:);
                 dist = 0;
-                for a=1:7
-                    for b=1:7
-                        dist = dist + (patch(a,b)-cut(a,b))^2;
-                    end
+                for a=1:i3
+                   dist = dist + (patch(a)-cut(a))^2;
                 end
                 dist = sqrt(dist);
                 if (dist < shortest_dist)
                     shortest_dist = dist;
-                    shortest_name = ['new_',num2str(x),'_',num2str(y),'.txt'];
+                    short = x;
                 end
-            end
         end
         % We have found shortest cut with name 'shortest_name' and dist
         % 'shortest_dist'. We then add each pixel value.
-        newPatch = load(shortest_name);
-        dictImg(i:(i+6),j:(j+6)) = dictImg(i:(i+6),j:(j+6))+newPatch;
+        newPatch = mat(short,2,:);
+        newPatch = reshape(newPatch,1,i3);
+        newPatch = reshape(newPatch,7,7);
+        NewI(i:(i+6),j:(j+6)) = NewI(i:(i+6),j:(j+6))+newPatch;
     end
 end
 
 figure(n); n=n+1;
-imagesc(dictImg); axis image; colormap(gray); colorbar
+imagesc(NewI); axis image; colormap(gray); colorbar
 set(gca,'fontsize',fntsz);
-title('dictImg');
+title('New image from dict');
 
 % DO CROSS REFERENCE HERE
-crossImg = zeros(size(dictImg));
-for i = 1:h
+crossImg = zeros(size(NewI));
+% for i = 1:h
+%     for j = 1:w
+%         if(convImg(i,j) > 0 && NewI(i,j) > 17)
+%             crossImg(i,j) = 1;
+%         end
+%     end
+% end
+
+crossImg = convImg;
+for i= 1:h
     for j = 1:w
-        if(convImg(i,j) > 0 && dictImg(i,j) > 17)
+        if(NewI(i,j) > 20)
             crossImg(i,j) = 1;
         end
     end
--- DictOfAwesomeness.m
+++ DictOfAwesomeness.m
@@ -1,24 +1,20 @@
 function [ ] = DictOfAwesomeness( )
-
     close all;
     clear all;
+    count = 0;
+    base = '/Users/claesladefoged/Dropbox/Skole/3/Bachelor/Billeder/csgb/';
+    %base = '/home/marcus/Dropbox/Bachelor/Billeder/csgb/';
+    
     n=1;
     fntsz=18;
     
     width = 35;
     height = 28;
-
-    %I = imread('/Users/claesladefoged/Dropbox/Skole/3/Bachelor/Billeder/csgb/test20_58.jpg');
-    %I = double(I(400+[0:255],1100+[0:255]));
     
-    I = imread('/home/marcus/Dropbox/Bachelor/Billeder/csgb/test20_58.jpg');
+    I = imread(strcat(base,'test20_58.jpg'));
     I = double(I(465+[0:height-1],1100+[0:width-1]));
     
-    %I = imread('/Users/claesladefoged/Dropbox/Skole/3/Bachelor/Billeder/csgb/test20_58.jpg');
-    %5I = double(I(467+[0:height-1],1108+[0:width-1]));
-    
     NewI = I.*0;
-
 
     figure(n); n=n+1;
     imagesc(I); axis image; colormap(gray); colorbar
@@ -30,48 +26,29 @@ function [ ] = DictOfAwesomeness( )
     figure(n); n=n+1;
     imagesc(abe); axis image; colormap(gray); colorbar
     set(gca,'fontsize',fntsz);
-    title('Subset image');
-    
-    %[x,y] = ginput(2);
-    
-    %xc = x(1);
-    %yc = y(1);
-    %radi = sqrt((x(1)-x(2))^2+(y(1)-y(2))^2);
-
-%     for i = 1:(height-1)
-%         for j = 1:(width-1)
-%             if(sqrt((i-yc)^2+(j-xc)^2) <= radi)
-%                 NewI(i,j) = 1;
-%             end
-%         end
-%     end
-
-    
+    title('Subset image');    
     
     for i = 0:(height/7-1)
         for j = 0:(width/7-1)
+            count = count + 1;
             y_from = i*7+1;
             y_to = i*7+7;
             x_from = j*7+1;
             x_to = j*7+7;
             
-            cut_original = double(I(y_from:y_to,x_from:x_to))
+            cut_original = double(I(y_from:y_to,x_from:x_to));            
             
-            %newname1 = ['orig_',num2str(i),'_',num2str(j),'.txt'];
-            %dlmwrite(newname1, cut_original, 'delimiter', '\t', 'precision', 6);
             cut_colored = double(abe(y_from:y_to,x_from:x_to));
-            %newname2 = ['new_',num2str(i),'_',num2str(j),'.txt'];
-            %dlmwrite(newname2, cut_colored, 'delimiter', '\t', 'precision', 6); 
             [x,y] = size(cut_colored);
             [x1,y1] = size(cut_original);
             var1 = reshape(cut_colored,1,x*y);
             var2 = reshape(cut_original,1,x1*y1);
             
-            mat(i+1,j+1,1,:) = var2;
-            mat(i+1,j+1,2,:) = var1;
+            mat(count,1,:) = var2;
+            mat(count,2,:) = var1;
         end
     end
-    save('minmis');
+    save('minmis','mat');
     close all;
 
 end
