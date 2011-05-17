192e24587bd21d6d872565a9f4d44a993d013831
Marcus <mgmano@gmail.com>
2011-05-17 12:50:09 +0200

    Some stuff

--- claes.m
+++ claes.m
@@ -3,35 +3,56 @@ clear all;
 n=1;
 percentage=0.35;
 %percentage=0.20;
+base1 = '/home/marcus/Dropbox/Bachelor/Billeder/csgb/';
 fntsz=18;
-I = imread('/Users/claesladefoged/Dropbox/Skole/3/Bachelor/Billeder/csgb/test20_48.jpg');
-I = double(I(200+[0:255],100+[0:255]));
-%I = double(imread('/Users/claesladefoged/Dropbox/Skole/3/Bachelor/Billeder/celle1.png'));
+I = double(imread(strcat(base1,'test20_58.jpg')));
 figure(n); n=n+1;
 imagesc(I); axis image; colormap(gray); colorbar
 set(gca,'fontsize',fntsz);
 title('Subset image');
+[x y] = ginput(2);
 
-listOfVessikels = [
-    '/Users/claesladefoged/Desktop/celle1.png',
-    '/Users/claesladefoged/Desktop/celle2.png'];
+lx = round(x(1));
+ly = round(y(1));
+rx = round(x(2));
+ry = round(y(2));
 
-iMax = size(listOfVessikels,1);
-for i = 1:(size(listOfVessikels,1))
-    J = double(imread(listOfVessikels(i,:)));
+I = I(ly:ry,lx:rx);
+figure(n); n=n+1;
+imagesc(double(I)); axis image; colormap(gray); colorbar
+set(gca,'fontsize',fntsz);
+title('Cuttet image');
+
+%gausskerne = fspecial('gaussian',[5,5],2)
+%gausskerne = Edge(I,'gauss')
+
+
+%gaussres = filter2(gausskerne,I);'
+for i = 1:20
+    gaussres = EDGE(I,'log',i);
+    size(gaussres);
     figure(n); n=n+1;
-    imagesc(J); axis image; colormap(gray); colorbar
-    set(gca,'fontsize',fntsz);
-    title('Template image');
-    fun = @(x) sum(((x(:)-mean(x(:)))/std(x(:))-(J(:)-mean(J(:)))/std(J(:))).^2);
-    K=nlfilter(I,size(J),fun);
-    figure(n); n=n+1;
-    imagesc(K); axis image; colormap(gray); colorbar
-    set(gca,'fontsize',fntsz);
-    title('Distance map');
-    figure(n); n=n+1;
-    blackNWhiteImg = (K-min(K(:)))<percentage*(max(K(:))-min(K(:)));
-    imagesc(blackNWhiteImg); axis image; colormap(gray); colorbar
-    set(gca,'fontsize',fntsz);
-    title(sprintf('Smallest %d%% in distance map',round(percentage*100)));
+    imagesc(gaussres); axis image; colormap(gray); colorbar
 end
+%[testval1, testval2] = ginput(2);
+
+% 
+% iMax = size(listOfVessikels,1);
+% for i = 1:(size(listOfVessikels,1))
+%     J = double(imread(listOfVessikels(i,:)));
+%     figure(n); n=n+1;
+%     imagesc(J); axis image; colormap(gray); colorbar
+%     set(gca,'fontsize',fntsz);
+%     title('Template image');
+%     fun = @(x) sum(((x(:)-mean(x(:)))/std(x(:))-(J(:)-mean(J(:)))/std(J(:))).^2);
+%     K=nlfilter(I,size(J),fun);
+%     figure(n); n=n+1;
+%     imagesc(K); axis image; colormap(gray); colorbar
+%     set(gca,'fontsize',fntsz);
+%     title('Distance map');
+%     figure(n); n=n+1;
+%     blackNWhiteImg = (K-min(K(:)))<percentage*(max(K(:))-min(K(:)));
+%     imagesc(blackNWhiteImg); axis image; colormap(gray); colorbar
+%     set(gca,'fontsize',fntsz);
+%     title(sprintf('Smallest %d%% in distance map',round(percentage*100)));
+% end
